9fd75537b33ef67882a3759edf536fcf7d0ede41
fabiobatalha <fabiobatalha@gmail.com>
2014-09-24 14:24:19 -0300

    Ajustes em gráfico de linhas

--- gchart/controller.py
+++ gchart/controller.py
@@ -102,7 +102,7 @@ class Ratchet():
             del(accesses['code'])
         if 'other' in accesses:
             del(accesses['other'])
-        if 'journal' in accesses and not is_instance(journal, dict):
+        if 'journal' in accesses and not isinstance(journal, dict):
             del(accesses['other'])
 
         data = []
